00f4f0bd31f5061019f626d87bf8ba7acbeedf8f
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-06-22 13:29:06 -0500

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import categoriaRoutes from "./routes/categorias.routes.js"
+
+const app = express();
+console.log(app);
+
+app.set("port", 5000);
+
+//Routes
+app.use("/api/categorias",categoriaRoutes);
+export default app;
