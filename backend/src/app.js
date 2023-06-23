caefd1eda3c457874ac64f4bf0e4ef3e503e9627
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-06-23 13:26:52 -0500

    FUNCIONA EL JSON EN LA PAGINAWEB

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,8 @@ import categoriaRoutes from "./routes/categorias.routes.js"
 const app = express();
 console.log(app);
 
-app.set("port", 5000);
+
+app.set("port", 4000);
 
 //Routes
 app.use("/api/categorias",categoriaRoutes);
--- backend/src/controllers/categoria.controllers.js
+++ backend/src/controllers/categoria.controllers.js
@@ -1,7 +1,12 @@
-const getCategorias = (req,res) => {
-    res.json({"Categoria":"Electrodomesticos"});
+import getConnection from "./../db/database.js"
+
+const getCategorias = async (req,res) => {
+    const connection = await getConnection();
+    const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias");
+    console.log(result);
+    res.json(result)
 }
 
 export const methodsHTTP = {
     getCategorias
-}
+}   
