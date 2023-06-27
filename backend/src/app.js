7eacf4a7fbe31b2feb480478d3f26b8389326385
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-06-27 13:11:25 -0500

    METODO POST NODE JS, FACTURARTEMIS

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,10 @@ import categoriaRoutes from "./routes/categorias.routes.js"
 const app = express();
 console.log(app);
 
-
+//Middleware
 app.set("port", 4000);
+
+app.use(express.json());
 
 //Routes
 app.use("/api/categorias",categoriaRoutes);
