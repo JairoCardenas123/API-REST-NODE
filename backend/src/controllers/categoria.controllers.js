0d860aaf45da84295d48722884318679bb7c7b8a
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-06-29 13:24:40 -0500

    PUT, DELETE (no funciona PUT)

--- backend/src/controllers/categoria.controllers.js
+++ backend/src/controllers/categoria.controllers.js
@@ -13,15 +13,26 @@ const getCategorias = async (req,res) => {
     
 }
 
+const getCategoria = async (req,res) => {
+    try {
+        console.log(req.params);
+        const {id} = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias WHERE categoriaID = ?", id);
+        console.log(result);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.mensaje);
+    }
+    
+}
+
 const addCategories = async(req,res)=>{
     try {
         const {CategoriaNombre,Descripcion,Imagen} = (req.body);
 
-        const category ={
-            CategoriaNombre,
-            Descripcion,
-            Imagen
-        }       
+        const category ={CategoriaNombre,Descripcion,Imagen}       
         const connection = await getConnection();
 
         const result = await connection.query("INSERT INTO categorias SET ?",category)
@@ -31,9 +42,42 @@ const addCategories = async(req,res)=>{
         res.send(error.mensaje);
     }
 }
+const deleteCategoria = async (req,res) => {
+    try {
+    const {id} = req.params;
+    const connection = await getConnection();
+    const result = await connection.query("DELETE FROM categorias WHERE CategoriaID = ?", id);
+    console.log(result);
+    res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.mensaje);
+    }
+    
+}
+
+const updateCategories = async (req, res)=>{
+    try {
+        const {id} = req.params;
+
+        const {CategoriaNombre, Descripcion, Imagen} = req.body;
+
+        const category = {CategoriaNombre, Descripcion,Imagen}       
+        const connection = await getConnection();
+
+        const result = await connection.query("UPDATE categorias SET ? WHERE CategoriaID = ?",[category, id])
+        res.json(result)
+    } catch (error) {
+        res.status(500);
+        res.send(error.mensaje);
+    }
+}
 
 export const methodsHTTP = {
     getCategorias,
-    addCategories
+    addCategories,
+    getCategoria,
+    deleteCategoria,
+    updateCategories
 }   
 
--- backend/src/routes/categorias.routes.js
+++ backend/src/routes/categorias.routes.js
@@ -5,4 +5,13 @@ const router = Router();
 router.get("/",categoriaController.getCategorias);
 router.post("/",categoriaController.addCategories);
 
+//la ruta recibe un parametro
+router.get("/:id",categoriaController.getCategoria);
+//la ruta debe recibir como parametro el id de la categoria para eliminar
+router.delete("/:id",categoriaController.deleteCategoria);
+
+router.put("/:id", categoriaController.updateCategories);
+
+
+
 export default router;
